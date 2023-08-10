233c566891181bfcc7825c38ee2f275b3303f93f
alex barlow <alex.barlow@rocketmail.com>
2023-08-10 15:28:26 -0400

    created state for adding data to page

--- src/components/itemCard.tsx
+++ src/components/itemCard.tsx
@@ -1,88 +1,89 @@
-import React, { Component } from 'react'
-import { log } from 'console';
-import { gql, useQuery } from '@apollo/client';
-const https = require('https');
+import React, { use } from "react";
+import { useState, useEffect } from 'react';
+import { log } from "console";
+import { gql, useQuery } from "@apollo/client";
+const https = require("https");
 
-type Props = {}
-
-type State = {} 
-
-
-const query = JSON.stringify({
-query: `{
-  items(name: "Assault Rifle") {
-    id
-    shortName
-  sellFor {
-    price
-    currency
-    priceRUB
-    source
-  }
-  avg24hPrice
-	description
- 	category {
-    id
-    name
- 	}
-  }
-}`
-
-})
+// interface Data {
+//   id: number
+//   shortName: string
+// }
 
 
+ 
+  
 
-const options = {
-    method: 'POST',
-    hostname: 'api.tarkov.dev/graphql',
+function ItemCard() {
+  const [posts, setPosts] = useState([]);
+
+  const query = JSON.stringify({
+    query: `{
+      items(name: "Assault Rifle") {
+        id
+        shortName
+      sellFor {
+        price
+        currency
+        priceRUB
+        source
+      }
+      avg24hPrice
+      description
+       category {
+        id
+        name
+       }
+      }
+    }`,
+  });
+
+  const options = {
+    method: "POST",
+    hostname: "api.tarkov.dev/graphql",
     headers: {
-      'Content-Type': 'application/json',
-      'Content-Length': query.length,
+      "Content-Type": "application/json",
+      "Content-Length": query.length,
     },
-
   };
 
-const request = https.request(options, (res: any) => {
-  let data = '';
-  console.log(`statusCode: ${res}`);
-
-  res.on('data', (d: any) => {
-    data += d;
-  });
-  res.on('end', () => {
-    // console.log(JSON.parse(data));
-    const returnData = JSON.parse(data)
-    console.log(returnData.data.items);
+  useEffect(() => {
     
-  })
-})
-request.on('error', (error: any) => {
-  console.error(error);
-});
+  const request = https.request(options, (res: any) => {
+    let data = "";
 
-request.write(query);
-request.end();
- 
+    res.on("data", (d: any) => {
+      data += d;
+    });
+    res.on("end", () => {
+      // console.log(JSON.parse(data));
+      const returnData = JSON.parse(data)
+      const apiData = returnData.data.items
+      console.log(returnData);
+      console.log(apiData);
+      setPosts(apiData)
+    });
+  });
+  request.on("error", (error: any) => {
+    console.error(error);
+  });
 
-// fetch(request)
-//   .then(response => {
-//     return response.json();
-//   })
-//   .then(data => {
-//     console.log('Data:', data);
-//   })
-//   .catch(error => {
-//     console.error('Error:', error);
-//   });
+  request.write(query);
+  request.end(); 
+}, []);
 
-export class ItemCard extends Component<Props, State> {
-  state = {}
 
-  render() {
-    return (
-      <div>itemCard</div>
-    )
-  }
+
+  return (
+    <main>
+      <div>
+        {posts.map((item) => (
+          <ul key={item.id} style={{color: "#000", display: 'inline-flex'}}>
+            <li style={{display: "flex"}}>{item.id}</li>
+          </ul>
+        ))}
+      </div>
+    </main>
+  );
 }
 
 export default ItemCard;
